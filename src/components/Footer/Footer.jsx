58c60ece26304c81304ae4715ccd69226b7d2747
kévin lokoka <kevinlokoka@gmail.com>
2021-10-27 16:30:33 +0200

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,6 +11,7 @@ const Footer = () =>{
                 </ul>
                 <div className={styles.ligne}></div>
                 <ul className={styles.onglets}>
+                <li className={styles.lien}>Accueil</li>
                 <li className={styles.lien}>Galerie</li>
                 <li className={styles.lien}>A propos</li>
                 <li className={styles.lien}>Contact</li>
@@ -37,6 +38,20 @@ const Footer = () =>{
                 <li className={styles.lien}>frocusx@hotmail.fr</li>
                 </ul>
                 </div>
+
+                <div>
+                <ul className={styles.onglets}>
+                <li className={styles.titre}>LOGO</li>
+                </ul>
+                <div className={styles.ligne}></div>
+                <ul className={styles.onglets}>
+                <li className={styles.lien}>En cours...</li>
+                </ul>
+                </div>
+                </div>
+
+                <div className={styles.droit} >
+                © 2020 - Tous droits réservés                
                 </div>
 
 
