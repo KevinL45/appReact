df2d94c88e311c324b032d8f1e2a511df3831c9b
kévin lokoka <kevinlokoka@gmail.com>
2021-10-27 15:29:15 +0200

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,28 +4,42 @@ import styles from "./Footer.module.css"
 const Footer = () =>{
     return (
         <footer>
-            <div className={styles.footer}>
-                <h3>MENUS</h3>
+                <div className={styles.grille}>
+                <div>
                 <ul className={styles.onglets}>
-                <li className={styles.lien}>Accueil</li>
+                <li className={styles.titre}>MENU</li>
+                </ul>
+                <div className={styles.ligne}></div>
+                <ul className={styles.onglets}>
                 <li className={styles.lien}>Galerie</li>
                 <li className={styles.lien}>A propos</li>
                 <li className={styles.lien}>Contact</li>
                 </ul>
-            </div>
-            <div className={styles.footer}>
-                <h3>RESEAUX SOCIAUX</h3>
+                </div>
+
+                <div>
+                <ul className={styles.onglets}>
+                <li className={styles.titre}>RESEAUX SOCIAUX</li>
+                </ul>
+                <div className={styles.ligne}></div>
                 <ul className={styles.onglets}>
                 <li className={styles.lien}>Twitter</li>
                 <li className={styles.lien}>Youtube</li>
                 </ul>
-            </div>
-            <div className={styles.footer}>
-                <h3>ME CONTACTER</h3>
+                </div>
+
+                <div>
+                <ul className={styles.onglets}>
+                <li className={styles.titre}>ME CONTACTER</li>
+                </ul>
+                <div className={styles.ligne}></div>
                 <ul className={styles.onglets}>
                 <li className={styles.lien}>frocusx@hotmail.fr</li>
                 </ul>
-            </div>
+                </div>
+                </div>
+
+
         </footer>
 
     )
